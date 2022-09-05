742cbcd370ea4fa08ad7a820a37c7e03e0a3df08
navitha <navithareddy112@gmail.com>
2022-09-05 19:45:07 +0530

    [navitha] 1.LengthTwoLines

---
+++ LineComparisionUsingOops/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionUsingOops
+{
+    internal class Program
+    {
+       public  static void Main(string[] args)
+        {
+            Console.WriteLine("choose option 1.Lengthoftwo \t 2.Equal");
+            Console.WriteLine("Enter the number");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    LengthTwoLines.CalculateTwoLineLength();
+                    break;
+                case 2:
+                    EqualLength.LengthEqualCheck();
+                    break;
+                    Console.ReadLine();
+            }
+        }
+    }
+}
